f51fe80c5e040d87f60de414cbad409815c9bb26
Rosi2 <rosa.llanos.13@sansano.usm.cl>
2018-11-28 16:24:23 -0300

    imagenes en grid

--- app/src/main/java/com/example/hphp/spetcare/Welcome_Activity.java
+++ app/src/main/java/com/example/hphp/spetcare/Welcome_Activity.java
@@ -1,8 +1,16 @@
 package com.example.hphp.spetcare;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Welcome_Activity extends AppCompatActivity {
     public static final String user="names";
@@ -14,5 +22,61 @@ public class Welcome_Activity extends AppCompatActivity {
         txtUser =(TextView)findViewById(R.id.textser);
         String user = getIntent().getStringExtra("names");
         txtUser.setText("¡Bienvenido "+ user +"!");
+        setContentView(R.layout.activity_welcome);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Welcome_Activity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(360, 360));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.perro, R.drawable.gato,
+                R.drawable.hamster, R.drawable.ave
+
+        };
     }
 }
