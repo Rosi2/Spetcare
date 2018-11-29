659db19e980522af1fa97e88895d328b2aad080e
Rosi2 <rosa.llanos.13@sansano.usm.cl>
2018-11-28 22:22:17 -0300

    clase para bd

---
+++ app/src/main/java/com/example/hphp/spetcare/datos.java
@@ -0,0 +1,20 @@
+package com.example.hphp.spetcare;
+
+public class datos {
+    private String p1;
+
+    public datos(){
+
+    }
+    public datos(String p1){
+        this.p1=p1;
+    }
+    public String getP1(){
+        return p1;
+    }
+    public void setP1(String p1){
+        this.p1=p1;
+    }
+
+}
+
